1b49f70960ad3c1150fa832e8ba6fa369eeceb6e
dniewinski <dniewinski@clearpathrobotics.com>
2018-06-29 10:59:14 -0400

    Update setup_ttb.bash

--- setup_ttb.bash
+++ setup_ttb.bash
@@ -4,6 +4,8 @@ wget -O enable_kernel_sources.sh http://bit.ly/en_krnl_src
 bash ./enable_kernel_sources.sh
 rm enable_kernel_sources.sh
 
+sudo apt-get update
+
 sudo apt-get install -y ros-kinetic-turtlebot ros-kinetic-turtlebot-msgs ros-kinetic-turtlebot-apps ros-kinetic-turtlebot-interactions ros-kinetic-turtlebot-simulator ros-kinetic-kobuki ros-kinetic-kobuki-msgs ros-kinetic-yujin-ocs ros-kinetic-astra-*
 
 wget https://raw.githubusercontent.com/clearpathrobotics/turtlebot2_setup/master/56-orbbec-usb.rules

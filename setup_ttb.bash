fa32777564afda89799056d24e8920f9eb93d250
Chris I-B <civerachb@clearpathrobotics.com>
2020-03-25 15:37:59 -0400

    Remove the -u option from the add-apt-repository line

--- setup_ttb.bash
+++ setup_ttb.bash
@@ -28,7 +28,7 @@ rm enable_kernel_sources.sh
 # add Intel's RealSense repository; otherwise we get problems installing librealsense
 # see: https://github.com/IntelRealSense/librealsense/blob/master/doc/distribution_linux.md#installing-the-packages
 sudo apt-key adv --keyserver keys.gnupg.net --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE
-sudo add-apt-repository "deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo xenial main" -u
+sudo add-apt-repository "deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo xenial main"
 
 # re-update apt since we've just added sources!
 sudo apt-get update

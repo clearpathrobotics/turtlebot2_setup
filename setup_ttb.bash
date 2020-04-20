aec32bb7bd5c5d012b2527769307b5b588cd217f
Chris I-B <civerachb@clearpathrobotics.com>
2020-04-20 12:10:35 -0400

    Update the OS & ROS codenames for Ubuntu 18.04 + Melodic

--- setup_ttb.bash
+++ setup_ttb.bash
@@ -12,7 +12,7 @@
 # check that we are _not_ running as root
 # running this script as root is _very_ bad and will break things
 if [[ $EUID -eq 0 ]]; then
-   echo "This script must not be run as root" 
+   echo "This script must not be run as root"
    exit 1
 fi
 
@@ -28,7 +28,7 @@ rm enable_kernel_sources.sh
 # add Intel's RealSense repository; otherwise we get problems installing librealsense
 # see: https://github.com/IntelRealSense/librealsense/blob/master/doc/distribution_linux.md#installing-the-packages
 sudo apt-key adv --keyserver keys.gnupg.net --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE
-sudo add-apt-repository "deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo xenial main"
+sudo add-apt-repository "deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo bionic main"
 
 # re-update apt since we've just added sources!
 sudo apt-get update
@@ -40,7 +40,7 @@ sudo apt-get install -y nano bash-completion
 sudo apt-get install -y librealsense2-dkms librealsense2-utils librealsense2-dev
 
 # install the core turtlebot system + dependencies
-sudo apt-get install -y ros-kinetic-turtlebot ros-kinetic-turtlebot-msgs ros-kinetic-turtlebot-apps ros-kinetic-turtlebot-interactions ros-kinetic-turtlebot-simulator ros-kinetic-kobuki ros-kinetic-kobuki-msgs ros-kinetic-yujin-ocs ros-kinetic-astra-*
+sudo apt-get install -y ros-melodic-turtlebot ros-melodic-turtlebot-msgs ros-melodic-turtlebot-apps ros-melodic-turtlebot-interactions ros-melodic-turtlebot-simulator ros-melodic-kobuki ros-melodic-kobuki-msgs ros-melodic-yujin-ocs ros-melodic-astra-*
 
 # set up the udev rules for the Orbbec Astra family of cameras
 wget https://raw.githubusercontent.com/clearpathrobotics/turtlebot2_setup/master/56-orbbec-usb.rules
@@ -51,6 +51,6 @@ wget https://github.com/TurtleBot-Mfg/turtlebot-wallpapers/raw/master/root/usr/s
 gsettings set org.gnome.desktop.background picture-uri file:///home/turtlebot/Pictures/TurtleBot_Lite_by_IHE.jpg
 
 # modify bashrc to load ROS automatically & use the Astra camera
-echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
 echo "export TURTLEBOT_3D_SENSOR=astra" >> ~/.bashrc
 source ~/.bashrc
